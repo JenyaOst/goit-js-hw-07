f59fed028e16f29b0c93933f7c7a611da419d211
JenyaOst <gjhit922@gmail.com>
2023-09-19 16:56:19 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <li data-previev = "${preview}" class="gallery__item js-gallery-item">
+      <a class="gallery__link" href="${original}" rel = "noopener noreferrer nofollow " >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `;
+  })
+  .join("");
+gallery.innerHTML = galleryList;
+const gallaryImages = gallery.querySelectorAll(".gallery__image");
+gallaryImages.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    event.preventDefault();
+    const source = image.dataset.source;
+    const alt = image.alt;
+    const instance = basicLightbox.create(`
+// <div class="modal"> 
+//     <img class = gallery__image src="${source}" alt = "${alt}" />
+//     </div>
+// `);
+
+    instance.show();
+    
+  });
+});
+
 
 console.log(galleryItems);
